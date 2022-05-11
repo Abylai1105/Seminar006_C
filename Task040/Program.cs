08246b60f5346e50282d1a64414c2e9102bceedf
Abylai <aba.omar@mail.ru>
2022-05-11 20:35:58 +0600

    Task040

---
+++ Task040/Program.cs
@@ -0,0 +1,22 @@
+﻿//Задача 40: Напишите программу, которая принимает на вход три числа и проверяет, 
+//может ли существовать треугольник с сторонами такой длины.
+
+int InputNumber(string name)
+{
+    Console.WriteLine($"Введите число {name} ");
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+int a = InputNumber ("A");
+int b = InputNumber ("B");
+int c = InputNumber ("C");
+
+if (a<b+c && b<a+c && c<b+c)
+{
+    Console.WriteLine("Это треугольник");
+}
+else
+{
+    Console.WriteLine("Это не тругольник");
+}
