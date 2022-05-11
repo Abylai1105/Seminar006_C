dca1f500ed12421597317ea404bf2131ab6b2f68
Abylai <aba.omar@mail.ru>
2022-05-11 19:27:06 +0600

    Task039

--- Task039/Program.cs
+++ Task039/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] array = GetArray(6);
+WriteArray(array);
+for (int i = 0; i < array.Length / 2; i++)
+{
+    int temp = array[i];
+    array[i] = array[array.Length - 1 -i];
+    array[array.Length - 1 -i] = temp;
+}
+WriteArray(array);
+
+int[] GetArray(int length)
+{
+    int[] newArray = new int [length];
+    FillArray(newArray);
+    return newArray;
+}
+
+void FillArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(0, 10);
+    }
+}
+void WriteArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine(array);
+}
