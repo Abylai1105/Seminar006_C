3755f43d3b6f8ffcbec354fc5d69b6464f353381
Abylai <aba.omar@mail.ru>
2022-05-12 17:57:27 +0600

    Task045

---
+++ Task045/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 45: Напишите программу, которая будет создавать копию 
+//заданного массива с помощью поэлементного копирования.
+
+
+Console.WriteLine("Введите длину массива ");
+int length = Convert.ToInt32(Console.ReadLine());
+int [] Fibonacci = new int [length];
+
+FillArray(Fibonacci);
+WriteArray(Fibonacci);
+
+int [] newArray = new int [length];
+
+for (int i = 0; i < length; i++)
+{
+    newArray [i] = Fibonacci [i];
+}
+Console.WriteLine("Копия массива: ");
+WriteArray(newArray);
+
+void FillArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(0, 10);
+    }
+}
+void WriteArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine(array);
+}
